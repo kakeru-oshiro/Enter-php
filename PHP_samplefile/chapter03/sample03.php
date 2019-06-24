fb369655483ad386326b650745c269fb8cf981ca
kakekake <kakeru.oshiro1301@gmail.com>
2019-06-24 22:59:43 +0900

    page38-44 date関数の位置設定をやらないとwarningが出ることがある

--- PHP_samplefile/chapter03/sample03.php
+++ PHP_samplefile/chapter03/sample03.php
@@ -21,6 +21,30 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 print('現在は ' . date('G時 i分 s秒') . ' です');
+print("\n");
+print(date('s'));
+print("\n");
+print(date('i'));
+print("\n");
+print(date('G'));
+print('時');
+print(date('i'));
+print('分');
+print(date('s'));
+print('秒');
+print("\n");
+print('現在は');
+print(date('G時 i分 s秒'));
+print('です');
+print("\n");
+print("現在は date('G時 i分 s秒') です");
+print("\n");
+print("現在は".date('G時 i分 s秒'. "です。"));
+print("\n");
+print('1+1は'.(1+1).'です');
+print("\n");
+print('今日は'.date("Y年 n月 j日").'です');
+
 ?>
 </pre>
 </main>
