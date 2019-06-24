e8964b77bd345003839622b67417f8bd133a4328
kakekake <kakeru.oshiro1301@gmail.com>
2019-06-24 22:13:04 +0900

    page 32-38

---
+++ PHP_samplefile/chapter03/templatefile/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+/* ここに、PHPのプログラムを記述します */
+print("I'm studying");
+print ("\n");
+print('I\'m studying "php"');
+print ("\n");
+print('１行目の文章です。');
+print ("\n");
+print('2行目の文章です。');
+print ("\n");
+// エスケープシーケンスを使う場合は必ずダブルクォーテーション
+// シングルクォーテーションは完全にそのまま使用、ダブルクォーテーションは変数など、評価できるものはする。
+?>
+</pre>
+</main>
+</body>    
+</html>
--- sample01.php
+++ sample01.php
@@ -1,3 +1,3 @@
-<?php
-print('PHPを勉強中です');
-?>
+
+<p><?php print('PHPを勉強中です'); ?></p>
+<a href="<?php print('http://h2o-space.com'); ?>">タグの属性にphpを埋めこみました</a>
