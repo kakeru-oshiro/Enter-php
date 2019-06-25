76fe4cbb97855c7598e7a80639b2254833f87c5a
kakekake <kakeru.oshiro1301@gmail.com>
2019-06-25 23:26:38 +0900

    page57~61

--- PHP_samplefile/chapter03/sample07.php
+++ PHP_samplefile/chapter03/sample07.php
@@ -19,9 +19,36 @@
 <h2>1年後までのカレンダーを作成する</h2>
 <pre>
 <?php
-for ($i=1; $i<=365; $i++) {
-    $day = date('n/j(D)', strtotime('+' . $i . 'day'));
-    print ($day . "\n");
+// for ($i=1; $i<=365; $i++) {
+//     $day = date('n/j(D)', strtotime('+' . $i . 'day'));
+//     print ($day . "\n");
+// }
+// for ($i = 1; $i <= 365; $i++) {
+//     $day = date('n/j(D)', strtotime('+'.$i.'day'));
+//     print($day."\n");
+// }
+// $day = date('n/j(D)');
+// print($day."\n");
+// $time = time();
+// print($time."\n");
+// $day = date('n/j(D)', 86400);
+// print($day."\n");
+// $ieyasu = strtotime('1543/1/31');
+// $day_after_tomorrow = strtotime('+2day');
+// $day = date('n/j(D)', $day_after_tomorrow);
+// print($day."\n");
+// for ($i =1; $i <= 365; $i++) {
+//     $timestamp = strtotime('+'.$i.'day');
+//     $day = date('n/j(D)', $timestamp);
+//     print($day."\n");
+// }
+// 
+$i = 1;
+while ($i <= 365) {
+    $timestamp = strtotime('+'.$i.'day');
+    $day = date('n/j(D)', $timestamp);
+    print($day."\n");
+    $i++;
 }
 ?>
 </pre>
