3c9210281b45c0c8de89be934316d3792aff4624
kakekake <kakeru.oshiro1301@gmail.com>
2019-06-30 23:05:08 +0900

    page85~91 jsonファイル

--- PHP_samplefile/chapter03/sample16.php
+++ PHP_samplefile/chapter03/sample16.php
@@ -18,7 +18,7 @@
 <main>
 <h2>JSONを読み込む</h2>
 <pre>
-<?php
+<!-- <?php
 $file = file_get_contents('https://h2o-space.com/feed/json');
 $json = json_decode($file);
 
@@ -27,7 +27,40 @@ foreach ($json->items as $item) :
 ・<a href="<?php print($item->url); ?>"><?php print($item->title); ?></a>
 <?php
 endforeach;
+?> -->
+<?php
+$file = file_get_contents('https://h2o-space.com/feed/json');
+$json = json_decode($file);
+
+foreach ($json->items as $item) :
 ?>
+・<a href = "<?php print($item->url); ?>"><?php print($item->title); ?></a>
+<?php
+endforeach;
+?>
+
+<?php
+$json_sample = [
+    'title' => 'JSONサンプル',
+    'items' => [
+        'item01' => '1つめ',
+        'item02' => '2つめ',
+    ]
+    ];
+$file = json_encode($json_sample, JSON_UNESCAPED_UNICODE);
+file_put_contents('./json_sample.json', $file);
+?>
+<?php
+$file = file_get_contents('./json_sample.json');
+$json = json_decode($file);
+
+foreach ($json->items as $item) :
+?>
+・<h3><?php print($item); ?></h3>
+<?php
+endforeach;
+?>
+
 </pre>
 </main>
 </body>
