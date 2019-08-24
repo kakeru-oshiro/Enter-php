f84b8732e391d348ed96c1ee3abbb47b729c157d
kakekake <kakeru.oshiro1301@gmail.com>
2019-08-24 23:39:57 +0900

    page198~203 DBの接続、phpファイルからのSQL実行(exec文)

--- PHP_samplefile/chapter05/templatefile/index.php
+++ PHP_samplefile/chapter05/templatefile/index.php
@@ -20,8 +20,20 @@
 <pre>
 <?php
 /* ここに、PHPのプログラムを記述します */
+try {$db = new PDO ('mysql:dbname=mydb;host=127.0.0.1;charset=utf8', 'root','');
+} catch (PDOException $e) {
+    echo 'DB接続エラー: '.$e->getMessage();
+}
+$count = $db -> exec('INSERT INTO my_items SET maker_id=1, item_name="もも",
+price=210, keyword="缶詰,ピンク,甘い", sales=0, created="2018-01-23",
+modified="2018-01-23"');
+echo $count . '件のデータを挿入しました。';
+
+// $count = $db->exec('UPDATE my_items SET item_name = "白桃" WHERE id = 5');
+// echo $count . '件変更しました';
+
+// $count = $db->exec('DELETE FROM my_items WHERE id=5');
+// echo $count . '件削除しました';
 ?>
-</pre>
-</main>
 </body>    
 </html>
