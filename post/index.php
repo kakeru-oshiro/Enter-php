6fd823b026cf4edd8cbf84b86d8078e29d34b16c
kakekake <kakeru.oshiro1301@gmail.com>
2019-10-09 23:31:25 +0900

    add

--- post/index.php
+++ post/index.php
@@ -31,7 +31,43 @@ if (!empty($_POST)) {
 }
 
 // 投稿を取得する
-$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+$page = $_REQUEST['page'];
+if ($page == '') {
+    $page = 1;
+}
+$page = max ($page, 1);
+
+// 最終ページを取得する
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxpage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxpage);
+
+$start = ($page - 1) * 5;
+$start = max(0, $start);
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
+
+
+// 最終ページを取得する
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxPage);
+
+$start = ($page - 1) * 5;
+$start = max(0, $start);
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
+
+
+
+
+
 
 // 返信の場合
 if (isset($_REQUEST['res'])) {
@@ -67,6 +103,7 @@ function makeLink($value) {
     <div id="head">
         <h1>ひとこと掲示板</h1>
     </div>
+    <div style="text-align: right"><a href="logout.php">ログアウト</a></div>
     <form action="" method="post">
         <dl>
             <dt><?php echo h($member['name']); ?>さん、メッセージをどうぞ</dt>
@@ -96,8 +133,37 @@ function makeLink($value) {
         <?php
         endif;
         ?>
+        <?php
+        if ($_SESSION['id'] == $post['member_id']):
+        ?>
+            [<a href="delete.php?id=<?php echo h($post['id']); ?>" style="color:#F33;">削除</a>]
+        <?php endif; ?>
         </p>
     </div>
     <?php endforeach; ?>
+
+    <ul class="paging">
+        <?php
+        if ($page > 1) {
+            ?>
+            <li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+            <?php
+        } else {
+            ?>
+        <li>前のページへ</li>
+        <?php
+        }
+        ?>
+        <?php
+        if ($page < $maxPage) {
+            ?>
+        <li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+        <?php
+        } else {
+            ?>
+        <li>次のページへ</li><?php
+        }
+        ?>
+    </ul>
 </div>
 
